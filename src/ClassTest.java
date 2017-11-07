952729a8c5cc6ec7be4cdd0d3e299eb5afab8a1c
alexzhou <alexzhoucs@gmail.com>
2017-11-07 22:08:29 +0800

    class testing (cannot run)

--- src/ClassTest.java
+++ src/ClassTest.java
@@ -2,12 +2,46 @@ public class ClassTest
 {
     public void main(String[] args)
     {
-
-
+        People p1 = new People();
+        p1.total++;
+        p1.name = "alex";
+        p1.age = 19;
+        p1.addCat(p1.cat,"c1");
+        System.out.println(p1.cat.name + p1.cat.num + p1.cat.allNum + p1.cat.total);
     }
 }
 
-class CreatString
+class People
 {
+    String name;
+    int age;
+    static int total = 0;
+    Cat cat;
 
+    void addCat(Cat cat,String name)
+    {
+        cat = new Cat(name);
+        System.out.println("add a cat\n");
+    }
+
+
+
+
+}
+
+class Cat
+{
+    String name;
+    int age;
+    int num;
+    static int total = 0;
+    static int allNum = 1;
+
+    public Cat(String name)
+    {
+        this.name = name;
+        this.num = this.allNum;
+        this.allNum = this.allNum++;
+        this.total ++;
+    }
 }
